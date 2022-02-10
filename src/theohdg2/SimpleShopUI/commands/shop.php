2c2999bb57aae59447f9e8b6b63d1a6ba9a3e9a3
Achedon12 <82544800+leoderoin@users.noreply.github.com>
2022-02-10 23:30:44 +0100

    add files

--- src/theohdg2/SimpleShopUI/commands/shop.php
+++ src/theohdg2/SimpleShopUI/commands/shop.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace theohdg2\SimpleShopUI\commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Translatable;
+
+class shop extends Command{
+
+    public function __construct(string $name, Translatable|string $description = "", Translatable|string|null $usageMessage = null, array $aliases = []){
+        parent::__construct($name, $description, $usageMessage, $aliases);
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args){
+        // TODO: Implement execute() method.
+    }
+}
