950159ecaf51410e0155f0e36ee84f8d14cf5219
theohdg2 <theohdg2@gmail.com>
2022-02-11 21:21:50 +0100

    push

--- src/theohdg2/SimpleShopUI/SimpleShopUI.php
+++ src/theohdg2/SimpleShopUI/SimpleShopUI.php
@@ -8,7 +8,9 @@ use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\player\Player;
+use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
 use pocketmine\utils\Config;
 use theohdg2\SimpleShopUI\Commands\ShopCommand;
 
@@ -21,9 +23,18 @@ class SimpleShopUI extends PluginBase{
 
     private Config $config;
     private Config $shop;
+    private Plugin $moneyAPI;
 
     protected function onEnable(): void
     {
+        if(($api = Server::getInstance()->getPluginManager()->getPlugin("SimpleMoneyAPI")) instanceof Plugin){
+            $this->moneyAPI = $api;
+        }else{
+            //TODO trad ans link
+            $this->getServer()->getLogger()->alert("please install dependence: SimpleMoneyAPI (here link)");
+            $this->getServer()->getPluginManager()->disablePlugin($this);
+        }
+
         //define the insatnce of this
         self::$instance = $this;
         //create folder
@@ -44,6 +55,7 @@ class SimpleShopUI extends PluginBase{
         $this->shop = new Config($this->getDataFolder()."shop.json",Config::JSON);
         //register command
         $this->getServer()->getCommandMap()->register("simpleshopui",new ShopCommand());
+
     }
 
     /**
@@ -70,6 +82,14 @@ class SimpleShopUI extends PluginBase{
         return $this->shop;
     }
 
+    /**
+     * @return Plugin
+     */
+    public function getMoneyAPI(): Plugin
+    {
+        return $this->moneyAPI;
+    }
+
     public function getConfigLanguage(): Config{
         return new Config($this->getDataFolder()."Language/lang_".$this->getConfig()->get("default-lang","eng").".yml",Config::YAML);
     }
@@ -78,13 +98,65 @@ class SimpleShopUI extends PluginBase{
     ///////////API\FORM//////////////
 
     //////////ADMIN SECTION//////////
-    public function getAccueilAdminForm():Form{}
+    public function getAccueilAdminForm():Form{
+        $form = new SimpleForm(function(Player $player,int $data = null){
+            if($data === null){
+                if($this->getConfig()->get("quit-message-admin-onenabled",false)){
+                    $player->sendMessage($this->getConfig()->get("quit-admin-message",""));
+                }
+                return;
+            }
+            switch ($data){
+                case 0:
+                    $player->sendForm($this->getCreateCategoryForm());
+                    break;
+                case 1:
+                    $player->sendForm($this->getEditCategoryForm());
+                    break;
+                case 2:
+                    $player->sendForm($this->getDeleteCategoryForm());
+                    break;
+                case 3:
+                    $player->sendForm($this->getAddItemForm());
+                    break;
+                case 4:
+                    $player->sendForm($this->getEditItemForm());
+                    break;
+                case 5:
+                    $player->sendForm($this->getDeleteItemForm());
+                    break;
+            }
+        });
+        //TODO trad
+
+        $form->setTitle("Admin shop configurateur");
+        //////SECTION CATEGORY/////////////////////////
+        $form->addButton("Créer une catégory");
+        $form->addButton("Editer une catégory");
+        $form->addButton("Supprimé une category");
+        //////SECTION ITEM/////////////////////////
+        $form->addButton("Créer une item");
+        $form->addButton("Editer une item");
+        $form->addButton("Supprimé une item");
+        return $form;
+    }
     /////////CATEGORY////////////////
-    public function getCreateCategoryForm(): Form{}
+    public function getCreateCategoryForm(): Form{
+        $form = new CustomForm(function(Player $player,array$data = null){
+
+        });
+        //TODO trad
+        $form->setTitle("Admin shop create category");
+        foreach ($this->getShop()->getAll() as $name => $data){
+            if ($data[""])
+        }
+
+        return $form;
+    }
     public function getDeleteCategoryForm(): Form{}
     public function getEditCategoryForm(): Form{}
     ////////////ITEM//////////////////
-    public function getaddItemForm(): Form{}
+    public function getAddItemForm(): Form{}
     public function getDeleteItemForm(): Form{}
     public function getEditItemForm(): Form{}
 
@@ -200,8 +272,14 @@ class SimpleShopUI extends PluginBase{
                $item->setLore($data["lores"] ?? []);
                $item->setCustomName($data["custom_name"]);
                if($player->getInventory()->canAddItem($item)){
-                   //TODO mettre l'api d'economy
+                   if($this->getMoneyAPI()->reduceMoney($player->getName(),$data["price_for_min_count"]??1)){
                    $player->getInventory()->addItem($item);
+                   //TODO trad
+                       $player->sendMessage("vous venez d'acheter ".$item->getCustomName()??$item->getName()."x".$option[1]);
+                   }else{
+                       //TODO trad
+                       $player->sendMessage("vous n'avez pas assez de money");
+                   }
                }else{
                    //TODO trad
                    $player->sendMessage("vous n'avez pas assez de place dans votre inventaire");
@@ -215,7 +293,7 @@ class SimpleShopUI extends PluginBase{
         //TODO trad
         $form->setTitle("Acheter ".$data["custom_name"] ?? "cette item");
 
-        $form->addLabel($data["price_for_max_count"]." pour ".$data["max_count"]);
+        $form->addLabel($data["price_for_min_count"]." pour ".$data["max_count"]);
         $form->addSlider("nombre",$data["min_count"],$data["max_count"],1);
 
         return $form;
